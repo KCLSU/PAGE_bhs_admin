d7680593ec3033aaa457d56e6a2329a855f1c313
Kyle Robinson <KyleR@kclsu.org>
2018-12-07 17:39:11 +0000

    export default

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,4 +1,4 @@
-export const config = {
+const config = {
   apiKey: "AIzaSyC7o0tWg-RBuxib1zEv_v6eRE2DLO_HxDI",
   authDomain: "happy-sad-ui.firebaseapp.com",
   databaseURL: "https://happy-sad-ui.firebaseio.com",
@@ -6,3 +6,5 @@ export const config = {
   storageBucket: "happy-sad-ui.appspot.com",
   messagingSenderId: "363245085216"
 };
+
+export default config
