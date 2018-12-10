621a5fb864d0e6ec3995c3fcd3c7d777fba2afb7
Kyle Robinson <KyleR@kclsu.org>
2018-12-10 08:02:53 +0000

    bug fix

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -6,7 +6,7 @@ ref.orderByChild("name").equalTo("Achilles").on("child_added", function(snapshot
   console.log(snapshot.key);
   console.log(snapshot.key.name);
   console.log(snapshot.key[0]);
-  console.log(snapshot.val();
+  console.log(snapshot.val());
   // snapshot.forEach(function(data) {
   //   console.log(data)
   //   // console.log(`${data.name} has a twitter handle of ${data.twitter}. He has been described as: ${data.description}`)
