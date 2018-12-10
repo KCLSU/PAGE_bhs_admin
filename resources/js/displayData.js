bb79e3ba7f831eccbb3ea63685009f2ac4d93e2e
Kyle Robinson <KyleR@kclsu.org>
2018-12-10 07:31:39 +0000

    first query

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -1,1 +1,7 @@
 import {config} from './config.js'
+
+var ref = db.ref("artists");
+ref.orderByChild("name").equalTo("Achilles").on("child_added", function(snapshot) {
+  console.log("List all the Achilles")
+  console.log(snapshot.key);
+});
