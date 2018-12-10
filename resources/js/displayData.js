94925ccc108ad4e2bcaf90a396e7eeb9b9e95985
Kyle Robinson <KyleR@kclsu.org>
2018-12-10 07:41:54 +0000

    snapshot query refining

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -3,5 +3,6 @@ var db = firebase.database();
 var ref = db.ref("artists");
 ref.orderByChild("name").equalTo("Achilles").on("child_added", function(snapshot) {
   console.log("List all the Achilles")
-  console.log(snapshot.key);
+  console.log(snapshot.key.name);
+  console.log(snapshot.name);
 });
