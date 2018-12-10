1e6ce5fe69e885839e0369d8d4a328a4817fd035
Kyle Robinson <KyleR@kclsu.org>
2018-12-10 08:00:43 +0000

    get snapshot val

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -4,9 +4,11 @@ var ref = db.ref("artists");
 ref.orderByChild("name").equalTo("Achilles").on("child_added", function(snapshot) {
   console.log("List all the Achilles")
   console.log(snapshot.key);
-  console.log(snapshot);
-  snapshot.forEach(function(data) {
-    console.log(data)
-    // console.log(`${data.name} has a twitter handle of ${data.twitter}. He has been described as: ${data.description}`)
-  })
+  console.log(snapshot.key.name);
+  console.log(snapshot.key[0]);
+  console.log(snapshot.val();
+  // snapshot.forEach(function(data) {
+  //   console.log(data)
+  //   // console.log(`${data.name} has a twitter handle of ${data.twitter}. He has been described as: ${data.description}`)
+  // })
 });
