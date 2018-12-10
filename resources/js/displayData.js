6a464739facd1b2072572af3372ad069a14916c0
Kyle Robinson <KyleR@kclsu.org>
2018-12-10 07:46:51 +0000

    loop through snapshots

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -5,4 +5,7 @@ ref.orderByChild("name").equalTo("Achilles").on("child_added", function(snapshot
   console.log("List all the Achilles")
   console.log(snapshot.key.name);
   console.log(snapshot.name);
+  snapshot.forEach(function(data) {
+    console.log(`${data.name} has a twitter handle of ${data.twitter}. He has been described as: ${data.description}`)
+  })
 });
