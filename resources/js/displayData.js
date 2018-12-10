cab4d910693f1ab9254400a2c926c5e78e7e6dc6
Kyle Robinson <KyleR@kclsu.org>
2018-12-10 07:38:34 +0000

    define db

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -1,5 +1,5 @@
-import {config} from './config.js'
-
+import { config } from './config.js'
+var db = firebase.database();
 var ref = db.ref("artists");
 ref.orderByChild("name").equalTo("Achilles").on("child_added", function(snapshot) {
   console.log("List all the Achilles")
