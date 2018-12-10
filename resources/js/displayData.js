f14c5c5b3d4cb25f773e07950d3e9894c72bb37d
Kyle Robinson <KyleR@kclsu.org>
2018-12-10 07:51:30 +0000

    query bug

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -3,9 +3,9 @@ var db = firebase.database();
 var ref = db.ref("artists");
 ref.orderByChild("name").equalTo("Achilles").on("child_added", function(snapshot) {
   console.log("List all the Achilles")
-  console.log(snapshot.key.name);
-  console.log(snapshot.name);
-  snapshot.forEach(function(data) {
-    console.log(`${data.name} has a twitter handle of ${data.twitter}. He has been described as: ${data.description}`)
-  })
+  console.log(snapshot.key);
+  console.log(snapshot);
+  // snapshot.forEach(function(data) {
+  //   console.log(`${data.name} has a twitter handle of ${data.twitter}. He has been described as: ${data.description}`)
+  // })
 });
