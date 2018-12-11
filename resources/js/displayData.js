b635d31aeb70a187bb7d66ad97ab0e7fcc32cf1d
Kyle Robinson <KyleR@kclsu.org>
2018-12-11 08:01:14 +0000

    append child lis

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -2,15 +2,19 @@ import { config } from './config.js'
 var db = firebase.database();
 var ref = db.ref("artists");
 const artistNames = [];
+
+
+let upcoming = document.getElementById('upcoming-list')
+
 ref.orderByChild("name").equalTo("Achilles").on("child_added", function(snapshot) {
   console.log("List all the Achilles")
   console.log(snapshot.key);
-artistNames.push(snapshot.val().name);
+  artistNames.push(snapshot.val().name);
+  let liS = `<li>${snapshot.val().name}</li>`
+  let li = document.createElement('li');
+  li.innerHTML = liS;
+  upcoming.appendChild(li);
 
-  // snapshot.forEach(function(data) {
-  //   console.log(data)
-  //   // console.log(`${data.name} has a twitter handle of ${data.twitter}. He has been described as: ${data.description}`)
-  // })
 });
 
 console.log(artistNames);
