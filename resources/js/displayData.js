238037422c4214b5f3ed7b8db812748700dffcb9
Kyle Robinson <KyleR@kclsu.org>
2018-12-11 07:51:38 +0000

    names array

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -1,14 +1,16 @@
 import { config } from './config.js'
 var db = firebase.database();
 var ref = db.ref("artists");
+const artistNames = [];
 ref.orderByChild("name").equalTo("Achilles").on("child_added", function(snapshot) {
   console.log("List all the Achilles")
   console.log(snapshot.key);
-  console.log(snapshot.key.name);
-  console.log(snapshot.key[0]);
-  console.log(snapshot.val());
+artistNames.push(snapshot.val().name);
+
   // snapshot.forEach(function(data) {
   //   console.log(data)
   //   // console.log(`${data.name} has a twitter handle of ${data.twitter}. He has been described as: ${data.description}`)
   // })
 });
+
+console.log(artistNames);
