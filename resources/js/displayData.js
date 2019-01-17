ab66a23058f589c86be86d959a5cb426259de8dc
Kyle Robinson <KyleR@kclsu.org>
2019-01-17 16:15:20 +0000

    test categories

--- resources/js/displayData.js
+++ resources/js/displayData.js
@@ -19,11 +19,12 @@ function loadArtists(){
     console.log("snapshot key: " + snapshot.key);
     artistNames.push(snapshot.val().name);
     let name = snapshot.val().name;
+    let type = snapshot.val().type;
     let li = document.createElement('li');
     li.innerHTML = name;
-    li.addEventListener('click', editArtists(snapshot.val(), snapshot.key))
+    // li.addEventListener('click', editArtists(snapshot.val(), snapshot.key))
 
-    switch(name){
+    switch(type){
       case "upcoming" :
         upcoming.appendChild(li);
       break;
@@ -42,46 +43,47 @@ function loadArtists(){
       case "poets" :
         poets.appendChild(li);
       break;
-      
+      default: console.log("Artist does not fall into category")
+
     }
 
   });
 
   console.log(artistNames);
 
-  function editArtist(data, key){
-
-    let popUp = `
-      <div class="popup">
-        <i class="fas fa-times" onclick="hidePopUp()"></i>
-        <div data-key=${key} class="artist-info">
-          <input val=${data.name} id="name" placeholder= "Name..">
-          <input val=${data.description} id="edit-description" >
-          <input val=${data.facebook} id="edit-facebook" >
-          <input val=${data.twitter} id="edit-twitter" >
-          <input val=${data.instagram} id="edit-instagram" >
-          <input val=${data.website} id="edit-website" >
-        </div>
-        <div class="button">
-          <a class="update-button" onclick="updateArtist(${data})"> Update</a>
-          <a class="delete-button" onclick="deleteArtist(${key})"> Delete </a>
-        </div>
-      </div>
-      `
-
-      function deleteArtist(key){
-
-      }
-
-      function updateArtist(data){
-
-      }
-
-      function hidePopUp(){
-
-      }
-
-  }
+  // function editArtist(data, key){
+  //
+  //   let popUp = `
+  //     <div class="popup">
+  //       <i class="fas fa-times" onclick="hidePopUp()"></i>
+  //       <div data-key=${key} class="artist-info">
+  //         <input val=${data.name} id="name" placeholder= "Name..">
+  //         <input val=${data.description} id="edit-description" >
+  //         <input val=${data.facebook} id="edit-facebook" >
+  //         <input val=${data.twitter} id="edit-twitter" >
+  //         <input val=${data.instagram} id="edit-instagram" >
+  //         <input val=${data.website} id="edit-website" >
+  //       </div>
+  //       <div class="button">
+  //         <a class="update-button" onclick="updateArtist(${data})"> Update</a>
+  //         <a class="delete-button" onclick="deleteArtist(${key})"> Delete </a>
+  //       </div>
+  //     </div>
+  //     `
+  //
+  //     function deleteArtist(key){
+  //
+  //     }
+  //
+  //     function updateArtist(data){
+  //
+  //     }
+  //
+  //     function hidePopUp(){
+  //
+  //     }
+  //
+  // }
 }
 
 loadArtists();
