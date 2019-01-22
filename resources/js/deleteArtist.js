88838c59d87bcb119ec97eb51d925f4780ed314d
Kyle Robinson <kylerobbo12@gmail.com>
2019-01-22 10:17:47 +0000

    remove inline onclick

--- resources/js/deleteArtist.js
+++ resources/js/deleteArtist.js
@@ -3,4 +3,5 @@ import { config } from './config.js';
 
 export function deleteArtist(data) {
   firebase.database().ref('/artists/' + data.key).remove();
+  console.log("artist removed: " + data.key)
 }
